d8d2230e437008bd098938fe191eea82b2821f60
otton <octaviogarciaguirre@gmail.com>
2015-11-07 02:44:00 -0300

    Initial commit

---
+++ searcher.py
@@ -0,0 +1,226 @@
+__author__ = 'otton'
+import json
+import requests
+from enum import Enum
+class steamtype(Enum):
+    error = 0
+    steamid = 1
+    steamid3 = 2
+    steamid64 = 3
+
+def getjson(steamid):#accepts only id64
+    return json.loads(requests.get("http://logs.tf/json_search?&player="+str(steamid)).content.decode())
+def getmatchjson(logid):
+    return json.loads(requests.get("http://logs.tf/json/"+str(logid)).content.decode())
+
+def idtoid64(steamid):#STEAM_0:A:B
+    A = int(steamid[8:9])
+    B = int(steamid[10:])
+    return 76561197960265728 + (B*2) + A
+def id3toid64(steamid3):#[U:1:B*2+A]
+    B_TIMES_2_PLUS_A=int(steamid3[5:len(steamid3)-1])
+    return 76561197960265728 + B_TIMES_2_PLUS_A
+
+def idtoid3(steamid):
+    A = int(steamid[8:9])
+    B = int(steamid[10:])
+    return "[U:1:"+(B*2+A).__str__()+"]"
+
+def id64toid3(steamid):
+    return "[U:1:"+steamid-76561197960265728+"]"
+
+def id3toid(steamid):
+    B_TIMES_2_PLUS_A=int(steamid[5:len(steamid)-1])
+    A=B_TIMES_2_PLUS_A%2
+    B=(B_TIMES_2_PLUS_A-A)/2
+    return "STEAM_0:"+A.__str__()+":"+B.__str__()
+def id64toid(steamid):
+    B_TIMES_2_PLUS_A=int(steamid)-76561197960265728
+    A=B_TIMES_2_PLUS_A%2
+    B=(B_TIMES_2_PLUS_A-A)/2
+    return "STEAM_0:"+A.__str__()+":"+B.__str__()
+
+def steamIdType(steamid):
+    type=steamtype.error
+    if steamid[:8]=="STEAM_0:":
+        type=steamtype.steamid
+    elif steamid[:5]=="[U:1:":
+        type=steamtype.steamid3
+    elif steamid.isdigit():
+        type=steamtype.steamid64
+    return type
+
+class statistic:
+    def __init__(self):
+        self.sum=0
+        self.squared_sum=0
+        self.n=0
+
+    def __repr__(self):
+        return self.__dict__
+    def __str__(self):
+        return "sum="+self.sum.__str__()+" squared_sum="+self.squared_sum.__str__()+" n="+self.n.__str__()
+class class_information:
+    def __init__(self,classname="Default"):
+        self.classname=classname
+        self.kills=statistic()
+        self.deaths=statistic()
+        self.assists=statistic()
+        self.dpm=statistic()
+
+    def __repr__(self):
+        return self.__dict__.__str__()
+    def __str__(self):
+        return "class=("+self.classname+") kills=("+self.kills.__str__()+") deaths=("+\
+            self.deaths.__str__()+") assists=("+self.assists.__str__()+") dpm=("+\
+            self.dpm.__str__()+")"
+
+if __name__=='__main__':
+    import sys
+    import datetime
+    import time
+
+    if(len(sys.argv)==1):
+        exit()
+    steamid=sys.argv[1]
+    date_begin=0
+    date_end=int(time.time())
+    debug=False
+    if(len(sys.argv)>=4):
+        date_begin=time.mktime(datetime.datetime.strptime(sys.argv[2],'%d/%m/%Y').timetuple())
+        date_end=time.mktime(datetime.datetime.strptime(sys.argv[3],'%d/%m/%Y').timetuple())
+        if(len(sys.argv)>=5):
+            debug=sys.argv[4]=="debug"
+
+        if debug:
+            print("Begin")
+            print(date_begin)
+            print("End")
+            print(date_end)
+    type=steamIdType(steamid)
+    if type==steamtype.error:
+        print("Steamid format is wrong",file=sys.stderr)
+        exit(0)
+    if type==steamtype.steamid:
+        pass
+    elif type==steamtype.steamid3:
+        steamid=id3toid(steamid)
+    elif type==steamtype.steamid64:
+        steamid=id64toid(steamid)
+
+    steamid3=idtoid3(steamid)
+    steamid64=idtoid64(steamid)
+
+    if debug:
+        print(steamid)
+        print(steamid3)
+        print(steamid64)
+
+
+
+    steamid3=idtoid3(steamid)
+
+    playerjson=getjson(steamid)
+
+
+    dict={}
+    dict["scout"]=class_information("scout")
+    dict["soldier"]=class_information("soldier")
+    dict["pyro"]=class_information("pyro")
+    dict["demoman"]=class_information("demoman")
+    dict["heavyweapons"]=class_information("heavyweapons")
+    dict["engineer"]=class_information("engineer")
+    dict["medic"]=class_information("medic")
+    dict["sniper"]=class_information("sniper")
+    dict["spy"]=class_information("spy")
+
+
+    completed=0
+    for i in playerjson["logs"]:
+        i_id = i["id"]
+        i_date = i["date"]
+        print("Looking at "+str(i_id), file=sys.stderr)
+        print("Date "+datetime.datetime.fromtimestamp(int(i_date)).strftime('%Y-%m-%d %H:%M:%S'),file=sys.stderr)
+
+        if(i_date>date_end):
+            print("Above the date limit, passing",file=sys.stderr)
+            continue
+
+        if(i_date<date_begin):
+            print("Under the date limit, stopping",file=sys.stderr)
+            break
+
+        match=getmatchjson(i_id)
+        idconverted=steamid
+        for id in match["players"]:#we dont really loop through we just need 1 element to check what type the log uses
+            match_id_type=steamIdType(id)
+            if match_id_type==steamtype.steamid:
+                idconverted=steamid
+            elif match_id_type==steamtype.steamid3:
+                idconverted=steamid3
+            elif match_id_type==steamtype.steamid64:
+                idconverted=steamid64
+            else:
+                print("Log Corrupted")
+                exit(1)
+            break
+
+        stats=match["players"][idconverted]
+        for stat in stats["class_stats"]:
+
+            class_name=stat["type"]
+            if class_name=="unknown" or class_name=="undefined":
+                continue
+
+            kills=int(stat["kills"])
+            deaths=int(stat["deaths"])
+            assists=int(stat["assists"])
+            n=float(stat["total_time"])/match["length"] #add the % of played time, againts the full match time
+
+            dict[class_name].kills.sum+=kills
+            dict[class_name].kills.squared_sum+=kills*kills
+            dict[class_name].kills.n+=n #amount of matches played with that class
+
+            dict[class_name].deaths.sum+=deaths
+            dict[class_name].deaths.squared_sum+=deaths*deaths
+            dict[class_name].deaths.n+=n
+
+            dict[class_name].assists.sum+=assists
+            dict[class_name].assists.squared_sum+=assists*assists
+            dict[class_name].assists.n+=n
+
+            if int(stat["total_time"])==0:#if you use a class less than a second we get a division by zero
+                continue
+            dpm=int(stat["dmg"])/int(stat["total_time"])
+            dpm*=60
+            dict[class_name].dpm.sum+=dpm
+            dict[class_name].dpm.squared_sum+=dpm*dpm
+            dict[class_name].dpm.n+=n
+
+        print("completed "+completed.__str__()+" of "+playerjson["results"].__str__(),file=sys.stderr)
+        completed+=1
+
+    print("{")
+    first=True
+    for i in dict:
+        if not first:
+            print(",")
+        else:
+            first=False
+
+        print("\""+i+"\":{")
+        print("\"kills\":")
+        print(json.dumps(dict[i].kills.__dict__))
+        print(",")
+
+        print("\"deaths\":")
+        print(json.dumps(dict[i].deaths.__dict__))
+        print(",")
+        print("\"assists\":")
+        print(json.dumps(dict[i].assists.__dict__))
+        print(",")
+        print("\"dpm\":")
+        print(json.dumps(dict[i].dpm.__dict__))
+
+        print("}")
+    print("}")
---
+++ statistics.py
@@ -0,0 +1,171 @@
+__author__ = 'otton'
+
+import subprocess
+import json
+import sys
+
+from PySide import QtGui
+
+def sqrt(x):
+    return x**(1.0/2.0)
+
+class Window(QtGui.QWidget):
+
+    def __init__(self,parent=None):
+        super(Window, self).__init__(parent)
+
+        self.initUI()
+
+    def initUI(self):
+
+
+        okButton = QtGui.QPushButton("GetStats")
+        cancelButton = QtGui.QPushButton("Cancel")
+        self.textbox = QtGui.QLineEdit()
+        #faster for testing stuff
+        self.textbox.setText("STEAM_0:1:24842999")
+
+
+        hbox = QtGui.QHBoxLayout()
+        self.beginCalendar=QtGui.QCalendarWidget()
+        self.endCalendar=QtGui.QCalendarWidget()
+        hbox.addWidget(self.beginCalendar)
+        hbox.addWidget(self.endCalendar)
+
+
+
+
+        vbox = QtGui.QVBoxLayout()
+        hbox2= QtGui.QHBoxLayout()
+        hbox2.addWidget(self.textbox)
+        hbox2.addWidget(okButton)
+        vbox.addWidget(QtGui.QLabel("Steamid:"))
+        vbox.addLayout(hbox2)
+        vbox.addLayout(hbox)
+        self.setLayout(vbox)
+
+        okButton.clicked.connect(self.okButtonClicked)
+        self.setGeometry(300, 300, 300, 150)
+        self.setWindowTitle('LogsAverager')
+        self.show()
+
+    def okButtonClicked(self):
+        print(self.textbox.text())
+        beginDate=self.beginCalendar.selectedDate().toString("dd/MM/yyyy")
+        endDate=self.endCalendar.selectedDate().toString("dd/MM/yyyy")
+
+        toExecute="python searcher.py "+self.textbox.text()+" "+beginDate+" "+endDate
+        print(toExecute)
+        output_s=subprocess.check_output(toExecute, shell=True)
+        output=json.loads(output_s.decode())
+        self.wid2=Window2()
+        self.wid2.setJson(output)
+
+
+
+class Window2(QtGui.QWidget):
+    def __init__(self,parent=None):
+        super(Window2, self).__init__(parent)
+        self.initUI()
+
+    def initUI(self):
+        self.table= QtGui.QTableWidget(9, 9, self)
+        self.columns=[
+        "KillsAverage","KillsSD",
+        "DeathsAverage","DeathsSD",
+        "AssistsAverage","AssistsSD",
+        "DPMAverage","DPMSD"
+        ,"Sample Size"
+        ]
+        self.rows=[
+        "scout","soldier","pyro",
+        "demoman","heavyweapons","engineer",
+        "medic","sniper","spy"
+        ]
+        self.table.setHorizontalHeaderLabels(self.columns)
+        self.table.setVerticalHeaderLabels(self.rows)
+        vbox = QtGui.QVBoxLayout()
+        self.table.setSizePolicy(QtGui.QSizePolicy.Minimum,QtGui.QSizePolicy.Minimum)
+        vbox.addWidget(self.table)
+        quitButton=QtGui.QPushButton("Quit")
+        quitButton.clicked.connect(self.hide)
+        vbox.addWidget(quitButton)
+
+        self.setLayout(vbox)
+        self.setGeometry(300, 300, 300, 150)
+        self.setWindowTitle('Table')
+
+        self.table.setItem(0,0,QtGui.QTableWidgetItem("test"))
+        self.table.itemClicked.connect(self.itemClicked)
+        self.show()
+
+    def setJson(self,json):
+        for row in range(0,self.table.rowCount()):
+            class_stat=json[self.rows[row]]
+
+            kills=class_stat["kills"]
+            killsAverage="-"
+            killsSD="-"
+            kills_n=float(kills["n"])
+            if(kills_n>0):
+                killsAverage=kills["sum"]/kills_n
+                killsSD=(kills["squared_sum"]/kills_n) - killsAverage**2
+                killsSD=sqrt(killsSD)
+                if(kills_n<15 and kills_n>1):#bessel correction
+                    killsSD*=sqrt(kills_n/(kills_n-1))
+
+            deaths=class_stat["deaths"]
+            deathsAverage="-"
+            deathsSD="-"
+            deaths_n=float(deaths["n"])
+            if(deaths_n>0):
+                deathsAverage=deaths["sum"]/deaths_n
+                deathsSD=(deaths["squared_sum"]/deaths_n) - deathsAverage**2
+                deathsSD=sqrt(deathsSD)
+                if(deaths_n<15 and deaths_n>1):
+                    deathsSD*=sqrt(deaths_n/(deaths_n-1))
+
+            assists=class_stat["assists"]
+            assistsAverage="-"
+            assistsSD="-"
+            assists_n=float(assists["n"])
+            if(assists_n>0):
+                assistsAverage=assists["sum"]/assists_n
+                assistsSD=(assists["squared_sum"]/assists_n) - assistsAverage**2
+                assistsSD=sqrt(assistsSD)
+                if(assists_n<15 and assists_n>1):
+                    assistsSD*=sqrt(assists_n/(assists_n-1))
+
+            dpm=class_stat["dpm"]
+            dpmAverage="-"
+            dpmSD="-"
+            dpm_n=float(dpm["n"])
+            if(dpm_n>0):
+                dpmAverage=dpm["sum"]/dpm_n
+                dpmSD=(dpm["squared_sum"]/dpm_n) - dpmAverage**2
+                dpmSD=sqrt(dpmSD)
+                if(dpm_n<15 and dpm_n>1):
+                    dpmSD*=sqrt(dpm_n/(dpm_n-1))
+
+            self.table.setItem(row,0,QtGui.QTableWidgetItem(str(killsAverage)))
+            self.table.setItem(row,1,QtGui.QTableWidgetItem(str(killsSD)))
+            self.table.setItem(row,2,QtGui.QTableWidgetItem(str(deathsAverage)))
+            self.table.setItem(row,3,QtGui.QTableWidgetItem(str(deathsSD)))
+            self.table.setItem(row,4,QtGui.QTableWidgetItem(str(assistsAverage)))
+            self.table.setItem(row,5,QtGui.QTableWidgetItem(str(assistsSD)))
+            self.table.setItem(row,6,QtGui.QTableWidgetItem(str(dpmAverage)))
+            self.table.setItem(row,7,QtGui.QTableWidgetItem(str(dpmSD)))
+            self.table.setItem(row,8,QtGui.QTableWidgetItem(str(dpm_n)))#all the n's should be equal, I need to deprecate this current json output from searchet.py
+
+    def itemClicked(self,item):
+        print("testt")
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+
+    wid = Window()
+    return app.exec_()
+
+if __name__=="__main__":
+    exit(main())
